9177cd97d8d2e12dc6bc96b38811b7661da00829
Kamil Grabowski <kambgrab@gmail.com>
2018-04-01 22:52:06 +0200

    fix typo in app.py file

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "My hotname is "+ str(socket.gethostbyname_ex(socket.gethostname()))
+	return "My hostname is "+ str(socket.gethostbyname_ex(socket.gethostname()))
